88f0319110f2ea5ab33b760ad15491c374e17b98
Ahmed khaled <93054790+Ahmed-Khaled-ibrahem@users.noreply.github.com>
2023-03-08 10:27:28 +0200

    product rating edit

--- lib/domain/data_objects/product_rating.dart
+++ lib/domain/data_objects/product_rating.dart
@@ -1,9 +1,11 @@
 class ProductRating {
   final double rating;
   final int quantity;
+  final String? temp ;
 
   ProductRating({
     required this.rating,
     required this.quantity,
+    this.temp,
   });
 }
